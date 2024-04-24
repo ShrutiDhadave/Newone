3165b09ca92d85049a11557264941d134cc809ae
ShrutiDhadave <167961334+ShrutiDhadave@users.noreply.github.com>
2024-04-24 13:36:40 +0530

    upload

---
+++ addition.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+     int a,b,c;
+     printf("Enter the two numbers");
+     scanf("%d%d",&a&b);
+     c=a+b;
+     printf("Addition is %d\n",c);
+     getch();
+}
